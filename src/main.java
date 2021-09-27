23431b5352a59c98298292307c05ae22d3db0b20
Avishay <avishaytsur1@gmail.com>
2021-09-27 18:14:13 +0300

    save

--- src/main.java
+++ src/main.java
@@ -1,6 +1,6 @@
 public class main {
     public static void main(String[] args) {
-        // Hello
+        System.out.println("A");
         TasksManager manager = TasksManager.getInstance();
     }
 }
